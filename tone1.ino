b4c350174242066a5b79618c3620764fb6467ce3
oae233 <61190251+oae233@users.noreply.github.com>
2020-02-25 15:56:02 +0400

    Add files via upload

---
+++ tone1.ino
@@ -0,0 +1,13 @@
+const int speakerPin = 8;
+const int frequency = 5000;
+
+void setup() {
+  // put your setup code here, to run once:
+tone(speakerPin, 400);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
